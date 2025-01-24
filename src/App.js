4d5703a2dd3b571ab596ba02d8ce9363b7999ed7
ChSaiJaswanth <21131A0534@gvpce.ac.in>
2025-01-24 21:25:35 +0530

    Modified index, app and app css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import SortingVisualizer from '../src/Components/SortingVisualizer';
+import React, { useState } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Original from '../src/Components/Original';
+import Header from '../src/Components/Header';
 import './App.css';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import RefreshHandler from './Components/RefreshHandler';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const PrivateRoute = ({element}) => {
+    return isAuth ? element : <Navigate to='/login' />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>SORTIFY - Interactive Sorting Visualizer</h1>
-      </header>
-      <SortingVisualizer />
-    </div>
+      <div className="App">
+        <RefreshHandler setIsAuth={setIsAuth}/>
+        <Routes>
+          <Route path='/' element={<Header />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/home' element={<PrivateRoute element={<Original />} />} />
+        </Routes>
+      </div>
   );
 }
 
